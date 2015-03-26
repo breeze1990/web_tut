b3aa0452327944a531002c7fd1a2d372a31f5c22
invisibili <breeze1990@gmail.com>
2015-03-26 19:57:49 -0400

    bug fix

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ var path = require('path');
 var http = require('http');
 var router = express();
 
+router.set('port',(process.env.PORT || 5000));
 router.use(express.static(path.resolve(__dirname, 'client'), {index:'comments.html'}));
 
 var server = http.createServer(router);
-server.listen(process.env.PORT || 3000, process.env.IP || "localhost", function(){
+server.listen(process.env.PORT || 5000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
   console.log("Chat server listening at", addr.address + ":" + addr.port);
 });
